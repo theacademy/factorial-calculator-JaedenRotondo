0d169f2f68fc7c3dc587e428725bcd8e7fa61553
Jaeden Rotondo <jae.rotondo@hotmail.ca>
2026-02-19 23:53:01 -0500

    implemented isInputNumberValid() and calculateFactorial

--- factorial/src/main/java/com/wiley/assignment1/Factorial.java
+++ factorial/src/main/java/com/wiley/assignment1/Factorial.java
@@ -35,7 +35,18 @@ public class Factorial {
 
 		//YOUR CODE STARTS HERE
 
-		return -1;
+		 if (scanner.hasNextInt()) {
+			 num = scanner.nextInt();
+		 } else {
+			 this.printStream.print("Invalid entry. Please enter an integer between 1 and 10, inclusive.");
+			 return -1;
+		 }
+		 if (num < 1 || num > 10) {
+			 this.printStream.print("Invalid entry. Please enter an integer between 1 and 10, inclusive.");
+			 return -1;
+		 }
+
+		 return num;
 
 		//YOUR CODE ENDS HERE
 		 
@@ -54,7 +65,9 @@ public class Factorial {
 		int result = 1;
 		//YOUR CODE STARTS HERE
 
- 
+	 	for (int i = 1; i <= num; i++) {
+			 result *= i;
+		 }
 
 		//YOUR CODE ENDS HERE
 		this.printStream.print("The Factorial is: " + result);		
